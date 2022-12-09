7d9b86e142a04ba5232de653e663bea04fcce1a5
Reforms <reformsgames@gmail.com>
2022-12-09 16:26:29 +0800

    change background color

--- scripts/config.py
+++ scripts/config.py
@@ -2,7 +2,7 @@
 class Config:
     window = {
         "caption": "title game",
-        "background_color": (0, 0, 0),
+        "background_color": (8, 9, 17),
         "base_resolution" : [1024, 576],
         "scaled_resolution" : [640, 360],
         "offset" : [0, 0],
